447201514df8d53341accb1d62af025b13854b5e
tr7077 <teorerras@gmail.com>
2025-07-14 14:52:46 +0300

    project setup - git init

---
+++ src/test/java/com/teorerras/buynowdotcom/BuynowdotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teorerras.buynowdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuynowdotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
